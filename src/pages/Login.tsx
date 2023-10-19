4751a2afb5115646330d8e5fd7c51700a8827c98
Nick <jakuradyhdyh@gmail.com>
2023-10-19 13:02:52 +1100

    new

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,3 +1,76 @@
+// import React, { useState } from 'react';
+
+// const Login = () => {
+//   const [username, setUsername] = useState('');
+//   const [password, setPassword] = useState('');
+//   const [isLoggedIn, setIsLoggedIn] = useState(false);
+//   const [errorMessage, setErrorMessage] = useState('');  // for displaying error messages
+
+//   const handleLogin = async () => {
+//     try {
+//       const response = await fetch('http://localhost:80/admin/user/login', {
+//         method: 'POST',
+//         headers: {
+//           'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify({ userName: username, password })
+//       });
+
+
+
+
+//       const data = await response.json();
+
+//       if (data.code === 200) {
+//         setIsLoggedIn(true);
+//         setErrorMessage(''); // Clear any previous error messages
+//       } else {
+//         setErrorMessage(data.msg);
+//       }
+
+//     } catch (error) {
+//       console.error("There was an error logging in:", error);
+//       setErrorMessage("An error occurred. Please try again.");
+//     }
+//   };
+
+//   const handleLogout = () => {
+//     setIsLoggedIn(false);
+//   };
+
+//   return (
+//     <div>
+//       {isLoggedIn ? (
+//         <div>
+//           <h2>Welcome, {username}!</h2>
+//           <button onClick={handleLogout}>Logout</button>
+//         </div>
+//       ) : (
+//         <div>
+//           <h2>Login</h2>
+//           {errorMessage && <div style={{ color: 'red' }}>{errorMessage}</div>} {/* Display error messages */}
+//           <input
+//             type="text"
+//             placeholder="Username"
+//             value={username}
+//             onChange={(e) => setUsername(e.target.value)}
+//           />
+//           <input
+//             type="password"
+//             placeholder="Password"
+//             value={password}
+//             onChange={(e) => setPassword(e.target.value)}
+//           />
+//           <button onClick={handleLogin}>Login</button>
+//         </div>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default Login;
+
+
 import React, { useState } from 'react';
 
 const Login = () => {
@@ -6,31 +79,12 @@ const Login = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [errorMessage, setErrorMessage] = useState('');  // for displaying error messages
 
-  const handleLogin = async () => {
-    try {
-      const response = await fetch('http://localhost:80/admin/user/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ userName: username, password })
-      });
-
-
-
-
-      const data = await response.json();
-
-      if (data.code === 200) {
-        setIsLoggedIn(true);
-        setErrorMessage(''); // Clear any previous error messages
-      } else {
-        setErrorMessage(data.msg);
-      }
-
-    } catch (error) {
-      console.error("There was an error logging in:", error);
-      setErrorMessage("An error occurred. Please try again.");
+  const handleLogin = () => {
+    if (username === 'username' && password === '123456') {
+      setIsLoggedIn(true);
+      setErrorMessage(''); // Clear any previous error messages
+    } else {
+      setErrorMessage('Invalid credentials. Please try again.');
     }
   };
 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,12 +8,14 @@ import {
   ColorScheme,
 } from "@mantine/core";
 import { Notifications } from "@mantine/notifications";
+import { UserProvider } from "@/contexts/UserContext"; // 引入我们之前创建的UserProvider
 export default function App({ Component, pageProps }: AppProps) {
   const [colorScheme, setColorScheme] = useState<ColorScheme>("light");
   const toggleColorScheme = (value?: ColorScheme) => {
     setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
   };
   return (
+    <UserProvider> {/* 添加 UserProvider */}
     <ColorSchemeProvider
       colorScheme={colorScheme}
       toggleColorScheme={toggleColorScheme}
@@ -27,5 +29,6 @@ export default function App({ Component, pageProps }: AppProps) {
         <Component {...pageProps} />
       </MantineProvider>
     </ColorSchemeProvider>
+    </UserProvider>
   );
 }
